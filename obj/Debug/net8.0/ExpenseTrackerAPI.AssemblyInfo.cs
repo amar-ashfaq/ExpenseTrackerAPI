84b64a2af10bc7d6be84f078d26501374b459f82
Amar AA <amar.ashfaq95@gmail.com>
2025-03-26 21:30:40 +0000

    Committing all files related to project setup

--- obj/Debug/net8.0/ExpenseTrackerAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/ExpenseTrackerAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTrackerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+227fa834d7ca33ef5f296788c639426f991a41d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTrackerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTrackerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
